a2638c5a3627f330c875ee4089e108d170b733b6
Aaron Bickhaus <aaron.bickhaus@gmail.com>
2020-04-24 11:17:18 -0400

    Adding Builder Pattern

---
+++ builders/BuilderPatternExamples/FacetedBuilder/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace FacetedBuilder
+{
+    public class Person
+    {
+        //address
+        public string StreetAddress, Postcode, City;
+
+        //employment
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{StreetAddress}, {Postcode}, {City},  {CompanyName}, {Position}, {AnnualIncome}";
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.Person = person;
+        }
+
+{        public PersonAddressBuilder At(string streetAddress)
+        {
+            Person.StreetAddress = streetAddress;
+            return this;
+        }
+
+        public PersonAddressBuilder WithPostCode(string postcode)
+        {
+            Person.Postcode = postcode;
+            return this;
+        }
+
+        public PersonAddressBuilder In(string city)
+        {
+            Person.City = city;
+            return this;
+        }
+    }
+
+    public class PersonBuilder //facade
+    {
+        //reference
+        protected Person Person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(Person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(Person);
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.Person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            Person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+             Person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            Person.AnnualIncome = amount;
+            return this;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var pb = new PersonBuilder();
+            var person = pb
+                .Lives.At("123 street")
+                .In("Denver")
+                .WithPostCode("123456")
+                .Works
+                .At("Heap Wolf")
+                .AsA("Engineer")
+                .Earning(100000);
+
+            Console.WriteLine(pb.ToString());
+        }
+    }
+}
---
+++ builders/BuilderPatternExamples/FluentBuilder/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FluentBuilder
+{
+    public class Person
+    {
+        public string Name, Position;
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)} : {Name}, {nameof(Position)} : {Position}";
+        }
+
+        public class Builder : PersonJobBuilder<Builder>
+        {
+
+        }
+
+        public static Builder New => new Builder();
+
+    }
+
+    public class PersonInforBuilder<SELF> : PersonBuilder where SELF : PersonInforBuilder<SELF>
+    {
+        
+
+        public SELF Called(string name)
+        {
+            person.Name = name;
+            return (SELF) this;
+        }
+    }
+
+    public abstract class PersonBuilder
+    {
+        protected Person person = new Person();
+
+        public Person Build()
+        {
+            return person;
+        }
+    }
+    public class PersonJobBuilder<SELF> : PersonInforBuilder<PersonJobBuilder<SELF>> where SELF : PersonJobBuilder<SELF>
+    {
+        public SELF WorkAsA(string position)
+        {
+            return (SELF)this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Person.New.Called("Aaron").WorkAsA("Software Engineer").Build());
+        }
+    }
+}
