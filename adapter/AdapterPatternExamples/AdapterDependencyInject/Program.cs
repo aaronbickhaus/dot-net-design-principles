3cbf267e98d633a4618cc85eb8c5f2736573df6c
Aaron Bickhaus <aaron.bickhaus@gmail.com>
2020-04-26 09:27:40 -0400

    adapterpattern

---
+++ adapter/AdapterPatternExamples/AdapterDependencyInject/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Autofac;
+using Autofac.Features.Metadata;
+
+namespace AdapterDependencyInject
+{
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    class SaveCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Save to file");
+        }
+    }
+
+    class OpenCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Open file");
+        }
+    }
+
+    public class Button
+    {
+        private ICommand command;
+        private string name;
+
+        public Button(ICommand command, string name)
+        {
+            this.command = command;
+            this.name = name;
+        }
+
+        public void PrintMe()
+        {
+            Console.WriteLine($"I am a button called {name}");
+        }
+
+        public void Click()
+        {
+            command.Execute();
+        }
+    }
+
+    public class Editor
+    {
+        private IEnumerable<Button> buttons;
+        public IEnumerable<Button> Buttons => buttons;
+
+        public Editor(IEnumerable<Button> buttons)
+        {
+            this.buttons = buttons;
+        }
+
+        public void ClickAll()
+        {
+            foreach(var button in buttons)
+            {
+                button.Click();
+            }
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var b = new ContainerBuilder();
+            b.RegisterType<SaveCommand>().As<ICommand>().WithMetadata("Name", "Save");
+            b.RegisterType<OpenCommand>().As<ICommand>().WithMetadata("Name", "Open"); ;
+            //registering as adapter here
+            b.RegisterAdapter<Meta<ICommand>, Button>(cmd => new Button(cmd.Value, (string)cmd.Metadata["Name"]));
+            b.RegisterType<Editor>();
+
+            using (var c = b.Build())
+            {
+                var editor = c.Resolve<Editor>();
+
+                foreach(var btn in editor.Buttons)
+                {
+                    btn.PrintMe();
+                }
+            }
+        }
+    }
+}
--- adapter/AdapterPatternExamples/VectorRasterExample/Program.cs
+++ adapter/AdapterPatternExamples/VectorRasterExample/Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace VectorRasterExample
 {
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+    }
+
+    public class VectorObject : Collection<Line>
+    {
+
+    }
+
+    public class Rectangle : VectorObject
+    {
+        public Rectangle(int x, int y, int width, int height)
+        {
+            Add(new Line(new Point(x, y), new Point(x + width, y)));
+        }
+    }
+
+    public class LineToPointAdapter : Collection<Point>
+    {
+        private static int count;
+
+        public LineToPointAdapter(Line line)
+        {
+            Console.WriteLine($"{++count}: {line.Start.X}  {line.Start.Y}");
+        }
+    }
+
     class Program
     {
+        private static readonly List<VectorObject> vectorObjects = new List<VectorObject>()
+        {
+            new Rectangle(1,1,10,10),
+            new Rectangle(1,1,10,10)
+        };
+       
+
+        public static void DrawPoint(Point p)
+        {
+            Console.WriteLine(".");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
